f8e85c54b09a925d47c53a51d6756316236f1a26
Анастасия Пенькова <nastya_22_06@mail.ru>
2023-10-08 13:49:06 +0300

    Вторая версия

--- hello.pas
+++ hello.pas
@@ -1,3 +1,32 @@
-﻿begin
-  Print('Привет, мир!')
+﻿//Дз 4
+(*программа спрашивала у пользователя язык, на котором он говорит и выводила приветствие на этом языке.*)
+begin
+  var a:string;
+  repeat 
+  a:=ReadString('Введите язык, на котором говорите');
+  if ((a <>'Русский') and (a<>'English') and (a <>'Italiano')) then
+    Println('Ошибка, введите Русский или English или Italiano');
+  until ((a ='Русский') or (a='English') or (a='Italiano'));
+   case a of
+    'Русский':Print('Привет');
+    'English':Print('Hello');
+    'Italiano':Print('Ciao');
+  end;
+  (*var a:=ReadString('Введите язык, на котором говорите');
+  Assert((a ='Русский') or (a='English') or (a ='Italiano'));
+  case a of
+    'Русский':Print('Привет');
+    'English':Print('Hello');
+    'Italiano':Print('Ciao');
+  end;*)
 end.
+
+(*Введите язык, на котором говорите Русский
+Привет *)
+(*Введите язык, на котором говорите English
+Hello *)
+(*Введите язык, на котором говорите Italiano
+Ciao *)
+(*Введите язык, на котором говорите Deutsch
+Ошибка, введите Русский или English или Italiano 
+Введите язык, на котором говорите *)
